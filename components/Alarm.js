55a8c329cdef0a53075ea7cf56d119fa4354b4e3
wennh <wennh@students.cs.ubc.ca>
2019-10-20 14:33:45 -0700

    converted seconds to mintes

---
+++ components/Alarm.js
@@ -0,0 +1,36 @@
+import React, { Component, useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import ProgressCircle from './ProgressCircle'
+
+export default class Alarm extends Component {
+  state = {
+    counting: true,
+    time: 120,
+    timeRemaining: 110,
+  };
+
+  countdown(){
+    if(this.state.timeRemaining-1 <= 0){
+      this.setState({timeRemaining: this.state.time});
+      clearInterval(this.interval);
+    }else{
+      this.setState({timeRemaining: this.state.timeRemaining - 1});
+    }
+  };
+
+  componentDidMount() {
+    console.log('here');
+    this.interval = setInterval(() => {
+       this.countdown();
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <View>
+        <ProgressCircle percentage={(1 - this.state.timeRemaining / this.state.time) * 100} 
+        seconds={this.state.timeRemaining}/>
+      </View>
+    );
+  }
+}
