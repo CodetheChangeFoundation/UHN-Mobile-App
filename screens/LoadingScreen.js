49950072c277e16ab39c6651d3982b70a9a15eb7
Maxine Yee <maxineyeee@gmail.com>
2019-10-26 13:07:00 -0700

    Added font loading in LoadingScreen

--- screens/LoadingScreen.js
+++ screens/LoadingScreen.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
   Text,
@@ -6,28 +6,55 @@ import {
   AsyncStorage
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
+import { AppLoading } from 'expo';
 
 const LoadingScreen = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
   useEffect(() => {
+    asyncLoadFonts();
     asyncCheckToken();
   })
+
+  asyncLoadFonts = async() => {
+    await Font.loadAsync({
+      // UHN requested fonts
+      'OpenSans-Regular' : require('../assets/fonts/OpenSans-Regular.ttf'),
+      'OpenSans-SemiBold' : require('../assets/fonts/OpenSans-SemiBold.ttf'),
+      'Prompt-Regular' : require('../assets/fonts/Prompt-Regular.ttf'),
+      // NativeBase fonts
+      Roboto : require('../node_modules/native-base/Fonts/Roboto.ttf'),
+      Roboto_medium : require('../node_modules/native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    setFontsLoaded(true);
+  }
 
   asyncCheckToken = async() => {
     const token = await AsyncStorage.getItem("token");
 
-    if (token) {
-      Actions.main();
-    } else {
-      Actions.auth();
+    if (fontsLoaded) {
+      if (token) {
+        Actions.main();
+      } else {
+        Actions.auth();
+      }
     }
   }
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>
-        Loading Screen
-      </Text>
-    </View>
-  );
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Loading Screen
+        </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
