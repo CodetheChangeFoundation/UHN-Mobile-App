1c06385686cce586eb3badfa0129bf9827173535
wennh <wennh@students.cs.ubc.ca>
2019-10-26 13:59:15 -0700

    fixed the format

--- components/Timer/Timer.js
+++ components/Timer/Timer.js
@@ -3,46 +3,45 @@ import { View } from 'react-native';
 import ProgressCircle from './ProgressCircle'
 
 export default class Timer extends Component {
-    state = {
-        counting: true,
-        time: 120,
-        timeRemaining: 120,
-    };
+  state = {
+    counting: true,
+    time: 120,
+    timeRemaining: 120,
+  };
 
-    countdown() {
-        if (this.state.timeRemaining - 1 <= 0) {
-            this.setState({ timeRemaining: 0 });
-            clearInterval(this.interval);
-        } else {
-            this.setState({ timeRemaining: this.state.timeRemaining - 1 });
-        }
-    };
-
-    incrementTimer() {
-        this.setState({ timeRemaining: this.state.timeRemaining + 15 });
-        this.setState({ time: this.state.timeRemaining + 15 });
-    };
-
-    decrementTimer() {
-        if (this.state.timeRemaining > 15) {
-            this.setState({ timeRemaining: this.state.timeRemaining - 15 });
-            this.setState({ time: this.state.timeRemaining - 15 });
-        }
-    };
-
-    componentDidMount() {
-        console.log('here');
-        this.interval = setInterval(() => {
-            this.countdown();
-        }, 1000);
-    };
-
-    render() {
-        return (
-            <View>
-                <ProgressCircle percentage={(1 - this.state.timeRemaining / this.state.time) * 100}
-                    seconds={this.state.timeRemaining} increaseTimeHandler={() => this.incrementTimer()} decreaseTimeHandler={() => this.decrementTimer()} />
-            </View>
-        );
+  countdown() {
+    if (this.state.timeRemaining - 1 <= 0) {
+      this.setState({ timeRemaining: 0 });
+      clearInterval(this.interval);
+    } else {
+      this.setState({ timeRemaining: this.state.timeRemaining - 1 });
     }
+  };
+
+  incrementTimer() {
+    this.setState({ timeRemaining: this.state.timeRemaining + 15 });
+    this.setState({ time: this.state.timeRemaining + 15 });
+  };
+
+  decrementTimer() {
+    if (this.state.timeRemaining > 15) {
+      this.setState({ timeRemaining: this.state.timeRemaining - 15 });
+      this.setState({ time: this.state.timeRemaining - 15 });
+    }
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.countdown();
+    }, 1000);
+  };
+
+  render() {
+    return (
+      <View>
+        <ProgressCircle percentage={(1 - this.state.timeRemaining / this.state.time) * 100}
+          seconds={this.state.timeRemaining} increaseTimeHandler={() => this.incrementTimer()} decreaseTimeHandler={() => this.decrementTimer()} />
+      </View>
+    );
+  }
 };
