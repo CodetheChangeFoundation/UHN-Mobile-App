87355a8a349d033696a41d8ef72c589f041df1c5
Maxine Yee <maxineyeee@gmail.com>
2019-10-26 13:06:13 -0700

    Added reusable components: Button, Input, Text, View

---
+++ components/text.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import theme from '../styles/base'
+import { Text as RNText, StyleSheet } from 'react-native';
+import { Title, Text as NBText } from 'native-base';
+
+export const Text = (props) => {
+  return (
+      <NBText style={[textStyles[props.variant], props.styles]}>{props.children}</NBText>
+  );
+}
+
+/* Styles */
+
+const body = {
+  fontFamily: theme.fonts.body,
+  fontSize: theme.fontSizes.medium,
+}
+
+const header = {
+  fontFamily: theme.fonts.header,
+  fontSize: theme.fontSizes.large,
+}
+
+const textStyles = StyleSheet.create({
+  body: {
+      ...body,
+      color: theme.colors.darkGrey,
+  },
+  header: {
+      ...header,
+      color: theme.colors.lightGrey,
+  },
+  primary: {
+      ...body,
+      color: theme.colors.white,
+  },
+  secondary: {
+      ...body,
+      color: theme.colors.lightGrey,
+  },
+  alarm: {
+      ...body,
+      fontSize: theme.fontSizes.large,
+      color: theme.colors.white,
+  },
+  urgent: {
+      ...body,
+      fontSize: theme.fontSizes.large,
+      color: theme.colors.white,
+  },
+});
---
+++ styles/base.js
@@ -0,0 +1,22 @@
+export const theme = {
+  colors : {
+    lightGrey: '#999B9E', 
+    darkGrey: '#67686B', 
+    green: '#60A781',
+    white: '#FFFFFF',
+    red: '#FF0000'
+  }, 
+  fonts : {
+    body: 'OpenSans-Regular',
+    header: 'OpenSans-SemiBold',
+    numeral: 'Prompt-Regular',
+  }, 
+  fontSizes : {
+    small: 16,
+    medium: 18,
+    large: 24,
+    xlarge: 72,
+  },
+}
+
+export default theme;
