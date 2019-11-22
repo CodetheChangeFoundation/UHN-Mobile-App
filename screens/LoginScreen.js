3bbec29c95d8f90f9ff074f2fde6369ed8e7132f
wennh <wenh_805@hotmail.com>
2019-11-21 18:39:12 -0800

    made backend request with login and signup

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,15 +1,14 @@
-import React, { Component, useState } from "react";
-import {
-  StyleSheet,
-} from "react-native";
+import React, { Component } from "react";
+import { StyleSheet } from "react-native";
 import { Actions } from "react-native-router-flux";
 import theme from "../styles/base"
 import { Container, Content, Header, View } from "../components/layout";
 import { Text } from "../components/typography";
 import { Button, Switch } from "../components/buttons";
 import { Form, Input } from "../components/forms";
+import * as axios from 'axios';
 
-export default class LoginForm extends Component {
+export default class LoginScreen extends Component {
 
   constructor(props) {
     super(props);
@@ -18,26 +17,46 @@ export default class LoginForm extends Component {
       password: "",
       error: "",
       loading: false,
-      rememberMe : false
+      rememberMe: false
     };
     this.setRememberMe = this.setRememberMe.bind(this);
+    this.onLoginButtonPress = this.onLoginButtonPress.bind(this);
+    this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    this.onLoginFailed = this.onLoginFailed.bind(this);
   }
 
-  setRememberMe(){
-    console.log("here");
-    if(this.state.rememberMe){
-      this.setState({rememberMe: false});
-    }else{
-      this.setState({rememberMe: true});
+  setRememberMe() {
+    if (this.state.rememberMe) {
+      this.setState({ rememberMe: false });
+    } else {
+      this.setState({ rememberMe: true });
     }
+  }
+
+  onLoginButtonPress() {
+    const { username, password } = this.state;
+    console.log("[DEBUG] LOGIN Button pressed.");
+    console.log("[DEBUG] username is " + username + ", password is " + password);
+
+    axios.post('http://localhost:3000/login', {
+      username: username,
+      password: password,
+    }).then(this.onLoginSuccess)
+    .catch(this.onLoginFailed);
+
+    Actions.main();
+  }
+
+  onLoginSuccess(response) {
+    console.log(response);
+  }
+
+  onLoginFailed(error) {
+    console.log(error);
   }
 
   render() {
     let passwordInputRef = React.createRef();
-    // _toggleRememberMe = (newValue) => {
-    //   setRememberMe(newValue);
-    // };
-    //const [rememberMe, setRememberMe] = useState(true);
 
     return (
       <Container>
@@ -50,7 +69,8 @@ export default class LoginForm extends Component {
                 label="Username"
                 hasNext
                 onChangeText={username => {
-                  console.log(username);
+                  this.setState({ username: username });
+                  console.log(this.state.username);
                 }}
                 onSubmitEditing={() => passwordInputRef._root.focus()}
               />
@@ -58,9 +78,10 @@ export default class LoginForm extends Component {
                 label="Password"
                 ref={(input) => passwordInputRef = input}
                 onChangeText={password => {
-                  console.log(password);
+                  this.setState({ password: password });
+                  console.log(this.state.password);
                 }}
-                onSubmitEditing={() => Actions.main()}
+                // onSubmitEditing={this.onLoginButtonPress}
               />
             </View>
 
@@ -70,7 +91,7 @@ export default class LoginForm extends Component {
             </View>
 
             <View style={styles.loginButton}>
-              <Button variant="primary" onPress={() => Actions.main()}>login</Button>
+              <Button variant="primary" onPress={this.onLoginButtonPress}>login</Button>
             </View>
 
             <View style={styles.signupButton}>
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -5,16 +5,56 @@ import { Container, Content, Header, View } from "../components/layout";
 import { Text } from "../components/typography";
 import { Button } from "../components/buttons";
 import { Form, Input } from "../components/forms";
+import * as axios from 'axios';
 
 export default class SignupScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       phoneNumber: "",
+      email: "",
       username: "",
       password: "",
+      loading: false
     }
+    this.onSignUpButtonPress = this.onSignUpButtonPress.bind(this);
+    this.onSignUpSuccess = this.onSignUpSuccess.bind(this);
+    this.onSignUpFailed = this.onSignUpFailed.bind(this);
   }
+
+  onSignUpButtonPress() {
+    const { email, phoneNumber, username, password } = this.state;
+    console.log("[DEBUG] SignUp Button pressed.");
+    console.log("[DEBUG] username is " + username + ", password is " + password + "\n email is " + email + " phoneNum is " + phoneNumber);
+    // TODO: generate error if username or password is empty string
+
+
+    // firebase.auth().signInWithEmailAndPassword(email, password)
+    //     .then(this.onLoginSuccess.bind(this))
+    //     .catch(() => {
+    //         firebase.auth().createUserWithEmailAndPassword(email,password)
+    //             .then(this.onLoginSuccess.bind(this))
+    //             .catch(this.onLoginFailed.bind(this));
+    //     });
+    axios.post('http://localhost:3000/signup', {
+      email: email,
+      phone: phoneNumber,
+      username: username,
+      password: password,
+    }).then(this.onSignUpSuccess)
+    .catch(this.onSignUpFailed);
+
+    Actions.login();
+  }
+
+  onSignUpSuccess(response) {
+    console.log(response);
+  }
+
+  onSignUpFailed(error) {
+    console.log(error);
+  }
+
   render() {
     let usernameInputRef = React.createRef();
     let passwordInputRef = React.createRef();
@@ -27,28 +67,49 @@ export default class SignupScreen extends Component {
             <View style={styles.title}>
               <Text variant="title">Create your account</Text>
             </View>
-
             <View style={styles.loginInfo}>
+              <Input variant="text"
+                label="Email"
+                hasNext
+                onChangeText={email => {
+                  this.setState({ email: email });
+                  console.log(this.state.email);
+                }}
+                onSubmitEditing={() => phoneNumberInputRef._root.focus()}
+              />
               <Input variant="number"
                 label="Phone Number"
+                ref={(input) => phoneNumberInputRef = input}
                 hasNext
+                onChangeText={phoneNumber => {
+                  this.setState({ phoneNumber: phoneNumber });
+                  console.log(this.state.phoneNumber);
+                }}
                 onSubmitEditing={() => usernameInputRef._root.focus()}
               />
               <Input variant="text"
                 label="Username"
                 ref={(input) => usernameInputRef = input}
                 hasNext
+                onChangeText={username => {
+                  this.setState({ username: username });
+                  console.log(this.state.username);
+                }}
                 onSubmitEditing={() => passwordInputRef._root.focus()}
               />
               <Input variant="text"
                 label="Password"
                 ref={(input) => passwordInputRef = input}
-                onSubmitEditing={() => Actions.login()}
+                onChangeText={password => {
+                  this.setState({ password: password });
+                  console.log(this.state.password);
+                }}
+              //onSubmitEditing={() => Actions.login()}
               />
             </View>
 
             <View style={styles.signupButton}>
-              <Button variant="primary" onPress={() => Actions.login()}>sign up</Button>
+              <Button variant="primary" onPress={this.onSignUpButtonPress}>sign up</Button>
             </View>
 
             <View style={styles.message}>
