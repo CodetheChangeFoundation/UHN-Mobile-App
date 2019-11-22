595ebdf421dc80c6dccf74d6cc3b4d95818560df
wennh <wenh_805@hotmail.com>
2019-11-21 16:19:28 -0800

    changed SignupScreen from functional component to class component

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,58 +1,66 @@
 import React, { Component } from "react";
-import {
-  StyleSheet,
-} from "react-native";
+import { StyleSheet } from "react-native";
 import { Actions } from "react-native-router-flux";
 import { Container, Content, Header, View } from "../components/layout";
 import { Text } from "../components/typography";
 import { Button } from "../components/buttons";
 import { Form, Input } from "../components/forms";
 
-const SignupScreen = () => {
-  let usernameInputRef = React.createRef();
-  let passwordInputRef = React.createRef();
+export default class SignupScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phoneNumber: "",
+      username: "",
+      password: "",
+    }
+  }
+  render() {
+    let usernameInputRef = React.createRef();
+    let passwordInputRef = React.createRef();
+    return (
+      <Container>
+        <Header>Sign Up</Header>
 
-  return (
-    <Container>
-    <Header>Sign Up</Header>
+        <Content>
+          <Form>
+            <View style={styles.title}>
+              <Text variant="title">Create your account</Text>
+            </View>
 
-    <Content>
-    <Form>
-      <View style={styles.title}>
-        <Text variant="title">Create your account</Text>
-      </View>
+            <View style={styles.loginInfo}>
+              <Input variant="number"
+                label="Phone Number"
+                hasNext
+                onSubmitEditing={() => usernameInputRef._root.focus()}
+              />
+              <Input variant="text"
+                label="Username"
+                ref={(input) => usernameInputRef = input}
+                hasNext
+                onSubmitEditing={() => passwordInputRef._root.focus()}
+              />
+              <Input variant="text"
+                label="Password"
+                ref={(input) => passwordInputRef = input}
+                onSubmitEditing={() => Actions.login()}
+              />
+            </View>
 
-      <View style={styles.loginInfo}>
-        <Input variant="number"
-          label="Phone Number"
-          hasNext
-          onSubmitEditing={() => usernameInputRef._root.focus()}
-        />
-        <Input variant="text"
-          label="Username"
-          ref={(input) => usernameInputRef = input}
-          hasNext
-          onSubmitEditing={() => passwordInputRef._root.focus()}
-        />
-        <Input variant="text"
-          label="Password"
-          ref={(input) => passwordInputRef = input}
-          onSubmitEditing={() => Actions.login()}
-        />
-      </View>
+            <View style={styles.signupButton}>
+              <Button variant="primary" onPress={() => Actions.login()}>sign up</Button>
+            </View>
 
-      <View style={styles.signupButton}>
-        <Button variant="primary" onPress={() => Actions.login()}>sign up</Button>
-      </View>
-
-      <View style={styles.message}>
-        <Text variant="footnote">You'll receive a verification code via text.</Text>
-      </View>
-    </Form>
-    </Content>
-    </Container>
-  );
+            <View style={styles.message}>
+              <Text variant="footnote">You'll receive a verification code via text.</Text>
+            </View>
+          </Form>
+        </Content>
+      </Container>
+    );
+  }
 }
+
 
 const styles = StyleSheet.create({
   title: {
@@ -69,5 +77,3 @@ const styles = StyleSheet.create({
     flex: 3,
   },
 });
-
-export default SignupScreen;
