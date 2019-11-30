cb9ee26e89b588de8a394be5fdf78c737227b3d9
wennh <wenh_805@hotmail.com>
2019-11-29 18:43:02 -0800

    forgot to commit this file

--- store/actions/Types.js
+++ store/actions/Types.js
@@ -1,5 +1,11 @@
+// TimerAction types
 export const INCREMENT_TIMER = "INCREMENT_TIMER";
 export const DECREMENT_TIMER = "DECREMENT_TIMER";
 export const COUNTDOWN = "COUNTDOWN";
 export const CLEARTIME = "CLEARTIME";
 export const RESETTIME = "RESETTIME";
+
+// AuthAction types
+export const LOGIN = "LOGIN";
+// export const SIGNUP = "SIGNUP";
+export const SET_LOADING = "SET_LOADING";
