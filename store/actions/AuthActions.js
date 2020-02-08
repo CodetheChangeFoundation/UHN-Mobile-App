8e74d1d53aef8205e77bcb6467b55977cff3f831
Asiapenguin <nicholaskong@live.ca>
2020-02-07 22:08:34 -0800

    Revert "Working handling of notifications, with some minor bugs"
    This reverts commit 2f11c5f81284090c8f5affd745fcf9f3a0abf311.

--- store/actions/AuthActions.js
+++ store/actions/AuthActions.js
@@ -3,7 +3,6 @@ import { LOGIN, LOGIN_FAILED, SIGNUP, SET_LOADING } from "./Types"
 import { SERVER_ROOT } from 'react-native-dotenv';
 import { Actions } from "react-native-router-flux";
 import { AsyncStorage } from "react-native";
-import { sendPushToken } from "../../services/push-token.service";
 
 
 const login = (data, rememberMe) => {
@@ -45,7 +44,6 @@ export const loginHandler = (credential, rememberMe) => {
       .then(async (response) => {
         dispatch(setLoading(false));
         dispatch(login(response.data, rememberMe));
-        sendPushToken(response.data.id);
         await AsyncStorage.setItem("token", response.data.token)
         Actions.main();
       })
