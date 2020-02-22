29a959048e3e633932d03d4e9457ff20ea07621a
Maxine Yee <maxineyeee@gmail.com>
2020-02-22 00:03:00 -0800

    Help request modal layout and styling

--- screens/modals/ResponderHelpRequestModal.js
+++ screens/modals/ResponderHelpRequestModal.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, AppState, Platform } from "react-native";
 import { Actions } from "react-native-router-flux";
 import theme from "../../styles/base";
@@ -6,39 +6,31 @@ import { Container, Content, Header, View } from "../../components/layout";
 import { Text } from "../../components/typography";
 import { Modal } from "../../components/popups";
 import { Button } from "../../components/buttons";
+import { convertToAddress } from "../../utils";
 
-const ResponderHelpRequestModal = props => {
+const ResponderHelpRequestModal = (props) => {
   if (
     AppState.currentState.match(/inactive|background/) &&
     Platform.OS !== "ios"
   )
     return null;
 
-  useEffect(() => {
-    console.log("notification received: ", JSON.stringify(props.notification));
-  });
-
   const userWhoNeedsHelp = props.notification.data.user;
   const modalHeader = `${userWhoNeedsHelp.username}\nis unresponsive`;
-  const modalBody = (
-    <View style={styles.buttons}>
-      <Button variant="affirmation" size="large"
-        onPress={() => acceptRequest()}
-      >
-        I am on my way with Naloxone
-      </Button>
-      <Button variant="light" size="large" 
-        onPress={() => declineRequest()}
-      >
-        I can't help now
-      </Button>
-    </View>
-  );
+  const [address, setAddress] = useState(null);
+
+  if (userWhoNeedsHelp.location && !address) {
+    convertToAddress(userWhoNeedsHelp.location.coords, setAddress);
+  }
+
+  useEffect(() => {
+    // console.log("notification received: ", JSON.stringify(props.notification));
+  });
 
   acceptRequest = () => {
     // TODO: hit help request endpoint to check if < 6 people have taken the request yet
     // if yes, then redirect to DirectionsScreen + pass notification data
-    // if no, alert saying the request has been taken & dismiss modal
+    // if no, alert saying the request has been taken, then dismiss modal
     Actions.assignment();
   }
 
@@ -46,11 +38,32 @@ const ResponderHelpRequestModal = props => {
     Actions.pop();
   }
 
+  renderModalBody = () => {
+    return (
+      <View style={styles.body}>
+        <Text style={styles.address}>{address || "Location not specified."}</Text>
+        <Button variant="affirmation" size="large"
+          onPress={() => acceptRequest()}
+        >
+          I am on my way with Naloxone
+        </Button>
+        <Button variant="light" size="large" 
+          style={styles.bottomButton}
+          onPress={() => declineRequest()}
+        >
+          I can't help now
+        </Button>
+      </View>
+    )
+  }
+
+  // Wait until location coordinates have been converted into an address
+  if (userWhoNeedsHelp.location && !address) return null;
   return (
     <Modal
       modalVisible={true}
       modalHeader={modalHeader}
-      modalBody={modalBody}
+      modalBody={renderModalBody()}
     />
   );
 };
@@ -58,8 +71,14 @@ const ResponderHelpRequestModal = props => {
 /* Styles */
 
 const styles = StyleSheet.create({
-  buttons: {
-    justifyContent: "space-around",
+  body: {
+    flex: 0,
+  },
+  address: {
+    textAlign: "center"
+  },
+  bottomButton: {
+    marginTop: 0,
   }
 })
 
