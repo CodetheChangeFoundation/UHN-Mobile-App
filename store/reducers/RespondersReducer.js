08f58baf5b391c2c2439a2e83799b84c5835f2e2
wennh <wenh_805@hotmail.com>
2020-02-23 23:40:25 -0800

    show list of responders

--- store/reducers/RespondersReducer.js
+++ store/reducers/RespondersReducer.js
@@ -1,13 +1,17 @@
-import { SET_MY_RESPONDERS, RESPONDERS_ERROR } from "../actions/Types"
+import { SET_MY_RESPONDERS, RESPONDERS_ERROR, SET_LOADING } from "../actions/Types"
 
 const initialState = {
-  myResponders: []
+  myResponders: [],
+  error: ""
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case SET_MY_RESPONDERS:
-      return { ...state, myResponders: action.data.myResponders };
+      console.log(action.data.myResponders);
+      return { ...state, myResponders: action.data.myResponders, error: "" };
+    case RESPONDERS_ERROR:
+      return { ...state, error: action.data.error.toString() };
     default:
       return state;
   }
