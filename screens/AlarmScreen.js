c0207e1575c21cba3b977158b6cd35befbf2d06d
andrchoi <andrew.tt.choi@gmail.com>
2020-02-29 09:38:39 -0800

    MA-49: updated alarmlog end time when exiting alarm screen

--- screens/AlarmScreen.js
+++ screens/AlarmScreen.js
@@ -26,7 +26,10 @@ const AlarmScreen = (props) => {
   
   const exitAlarm = () => {
     Alert.alert("Are you sure you want to exit?", "This will reset the timer", [
-      { text: 'Yes', onPress: () => Actions.main() },
+      { text: 'Yes', onPress: () => {
+        Actions.main();
+        props.updateAlarmLog(0, null, props.currentAlarmLog, token)
+      }},
       { text: 'No', style: 'cancel' }
     ], { cancelable: false });
   }
