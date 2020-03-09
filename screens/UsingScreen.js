e27dac60d634c6ebd62155032fe92c3d8caff87a
wennh <wenh_805@hotmail.com>
2020-03-08 20:58:55 -0700

    get the number of available responders on the Using Screen

--- screens/UsingScreen.js
+++ screens/UsingScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { StyleSheet, Alert } from "react-native";
 import { connect } from 'react-redux'
 import { Actions } from "react-native-router-flux";
@@ -6,7 +6,7 @@ import Timer from "../components/Timer/Timer";
 import { Container, Content, Header, View, Segment, Banner } from "../components/layout";
 import { Button, IconButton } from "../components/buttons";
 import { computeDistance } from '../utils'
-import { makeAlarmLog } from "../store/actions";
+import { makeAlarmLog, getNumberOfAvailableResponders } from "../store/actions";
 
 const fredVictorCoordinates = {
   lat: 43.6536212,
@@ -16,49 +16,60 @@ const fredVictorCoordinates = {
 // TODO: Remember to change this to 500 prior to Beta testing
 const MAXIMUM_DISTANCE = 1000000000 // meters
 
-const UsingScreen = (props) => {
 
-  const { time, userId, token } = props;
-  const { timeRemaining } = time
+class UsingScreen extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.startAlarm.bind(this);
+  }
 
-  const startAlarm = () => {
-    const distance = computeDistance(fredVictorCoordinates, props.location.coords)
-    if( distance > MAXIMUM_DISTANCE ) Alert.alert("Cannot start alarm", "There are no responders within your area", [
+  startAlarm = () => {
+    const { time, userId, token } = this.props;
+    const { timeRemaining } = time
+    const distance = computeDistance(fredVictorCoordinates, this.props.location.coords)
+    if (distance > MAXIMUM_DISTANCE) Alert.alert("Cannot start alarm", "There are no responders within your area", [
       { text: 'OK' }
     ], { cancelable: false })
     else {
       Actions.alarm();
 
-      props.makeAlarmLog(userId, timeRemaining, token);
+      this.props.makeAlarmLog(userId, timeRemaining, token);
     }
   }
 
-  return (
-    <Container>
-    <Header leftButton="menu" onLeftButtonPress={() => Actions.drawerOpen()}>Using Mode</Header>
+  componentDidMount(){
+    this.props.getNumberOfAvailableResponders(this.props.userId, this.props.token);
+  }
 
-    <Content>
-      <Banner>
-        <Segment active="left"
-          leftText="using" rightText="responding"
-          onRightButtonPress={() => Actions.responding()}
-        />
-      </Banner>
-      <Banner>
-        <IconButton variant="icon" name="md-pin" label="current location" onPress={() => Actions.location()}/>
-        <IconButton variant="counter" counterValue={3} label="responders available" onPress={() => Actions.responders()} />
-      </Banner>
-    
-      <View style={styles.timer}>
-        <Timer isUsing={false}/>
-      </View>
+  render() {
+    return (
+      <Container>
+        <Header leftButton="menu" onLeftButtonPress={() => Actions.drawerOpen()}>Using Mode</Header>
 
-      <View style={styles.startButton}>
-        <Button variant="affirmation" size="large" onPress={startAlarm}>start</Button>
-      </View>
-    </Content>
-    </Container>
-  );
+        <Content>
+          <Banner>
+            <Segment active="left"
+              leftText="using" rightText="responding"
+              onRightButtonPress={() => Actions.responding()}
+            />
+          </Banner>
+          <Banner>
+            <IconButton variant="icon" name="md-pin" label="current location" onPress={() => Actions.location()} />
+            <IconButton variant="counter" counterValue={this.props.respondersAvailable} label="responders available" onPress={() => Actions.responders()} />
+          </Banner>
+
+          <View style={styles.timer}>
+            <Timer isUsing={false} />
+          </View>
+
+          <View style={styles.startButton}>
+            <Button variant="affirmation" size="large" onPress={this.startAlarm}>start</Button>
+          </View>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -72,12 +83,13 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = (state, currentProps) => {
   const { location } = state.userData;
-  return { 
+  return {
     location,
     userId: state.auth.userId,
-    time: state.timer, 
-    token: state.auth.token
+    time: state.timer,
+    token: state.auth.token,
+    respondersAvailable: state.userData.respondersAvailable
   }
 }
 
-export default connect(mapStateToProps, { makeAlarmLog })(UsingScreen);
+export default connect(mapStateToProps, { makeAlarmLog, getNumberOfAvailableResponders })(UsingScreen);
