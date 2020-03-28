7a48cd235ea761eed6582b2fcbd825c9eb335251
Asiapenguin <nicholaskong@live.ca>
2020-03-28 13:30:23 -0700

    Spacing and alert scene changes

--- screens/AlarmScreen.js
+++ screens/AlarmScreen.js
@@ -2,33 +2,31 @@ import React, { useEffect } from "react";
 import { StyleSheet } from "react-native";
 import { connect } from "react-redux";
 import { Actions } from "react-native-router-flux";
-import Timer from "../components/Timer/Timer"
+import Timer from "../components/Timer/Timer";
 import { Container, Content, Header, View, Banner } from "../components/layout";
-import { Button } from "../components/buttons"
+import { Button } from "../components/buttons";
 import { Text } from "../components/typography";
 import { sendHelpRequest } from "../services/help-request.service";
 import { updateAlarmLog } from "../store/actions";
 
-const AlarmScreen = (props) => {
-
-  const { time, userId, token } = props
-  const { timeRemaining } = time
+const AlarmScreen = props => {
+  const { time, userId, token } = props;
+  const { timeRemaining } = time;
 
   const sendHelp = () => {
-    sendHelpRequest(userId, token)
-      .then((response) => {
-        Actions.alert({
-          alertTitle: "Help request sent",
-          alertBody: "A help request has been sent to your responder network.",
-          positiveButton: { text: "OK", onPress: () => Actions.main() },
-          cancelable: false 
-        });
-        
-        props.updateAlarmLog(0, true, props.currentAlarmLog, token)
-        Actions.main()
-      })
-  }
-  
+    sendHelpRequest(userId, token).then(response => {
+      Actions.alert({
+        alertTitle: "Help request sent",
+        alertBody: "A help request has been sent to your responder network.",
+        positiveButton: { text: "OK" },
+        cancelable: false
+      });
+
+      props.updateAlarmLog(0, true, props.currentAlarmLog, token);
+      Actions.main();
+    });
+  };
+
   const exitAlarm = () => {
     Actions.alert({
       alertTitle: "Are you sure you want to exit?",
@@ -37,7 +35,7 @@ const AlarmScreen = (props) => {
         text: "Yes",
         onPress: () => {
           Actions.main();
-          props.updateAlarmLog(0, null, props.currentAlarmLog, token)
+          props.updateAlarmLog(0, null, props.currentAlarmLog, token);
         }
       },
       negativeButton: {
@@ -46,37 +44,37 @@ const AlarmScreen = (props) => {
       },
       cancelable: false
     });
-  }
-  
+  };
+
   return (
     <Container>
-    <Header
-      leftButton="arrow" 
-      onLeftButtonPress={exitAlarm}
-    >Counting down</Header>
+      <Header leftButton="arrow" onLeftButtonPress={exitAlarm}>
+        Counting down
+      </Header>
 
-    <Content>
-      <Banner />
-      <Banner />
-      
-      <View style={styles.timer}>
-        <Timer isUsing={true}/>
-      </View>
+      <Content>
+        <Banner />
+        <Banner />
 
-      <View style={styles.helpButton}>
-        <Button variant="warning" size="large" onPress={sendHelp}>help now!</Button>
-      </View>
+        <View style={styles.timer}>
+          <Timer isUsing={true} />
+        </View>
 
-    </Content>
+        <View style={styles.helpButton}>
+          <Button variant="warning" size="large" onPress={sendHelp}>
+            help now!
+          </Button>
+        </View>
+      </Content>
     </Container>
   );
-}
+};
 
 const styles = StyleSheet.create({
   timer: {
     flex: 5,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   },
   helpButton: {
     flex: 2
@@ -89,7 +87,7 @@ function mapStateToProps(state) {
     userId: state.auth.userId,
     token: state.auth.token,
     currentAlarmLog: state.alarm.currentAlarmLog
-  }
+  };
 }
 
 export default connect(mapStateToProps, { updateAlarmLog })(AlarmScreen);
